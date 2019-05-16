907635f251f72fdeec5345374c959b53da80bcbd
M Banks <montensia1@outlook.com>
2019-05-16 18:03:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(array_of_words)
   array_of_words.any? {|word| word.length > 4}
 end
 
-def find_the_cheese(type_of_cheese)
+def find_the_cheese(cheeses)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
